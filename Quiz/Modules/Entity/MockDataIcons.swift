373a499619bfb2374252c7e4da2ee431a94ff569
Nikita Kornuk <kornyk62@gmail.com>
2025-02-11 20:54:06 +0100

    add enum for icons

--- Quiz/Modules/Entity/MockDataIcons.swift
+++ Quiz/Modules/Entity/MockDataIcons.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import UIKit
+
+enum Icons {
+    static let GermanyFlagIcon = UIImage(named: "Flag_of_Germany")
+    static let UnitedKingdomFlagIcon = UIImage(named: "Flag_of_United_Kingdom")
+    static let SpainFlagIcon = UIImage(named: "Flag_of_Spain")
+    static let FranceFlagIcon = UIImage(named: "Flag_of_France")
+}
