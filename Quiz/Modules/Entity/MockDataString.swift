42760d2692c848c49be50917260c0176a1628994
Nikita Kornuk <kornyk62@gmail.com>
2025-02-11 20:55:11 +0100

    add enum for mainView

--- Quiz/Modules/Entity/MockDataString.swift
+++ Quiz/Modules/Entity/MockDataString.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 enum StringsData {
-    static let titleNavigation = "Language quizzes"
-    static let correctAnswer = "Правильных ответов"
-    static let titleButtonOffline = "Оффлайн"
-    static let titleButtonOnline = "Онлайн"
+    enum MainView {
+        static let titleNavigation = "Language quizzes"
+        static let correctAnswer = "Правильных ответов"
+        static let titleButtonOffline = "Оффлайн"
+        static let titleButtonOnline = "Онлайн"
+    }
 }
 
