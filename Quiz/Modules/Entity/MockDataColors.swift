ee802295f9c76c98c5ef795890c5ba9a037b57c1
Nikita Kornuk <kornyk62@gmail.com>
2025-02-11 20:53:55 +0100

    change colors

--- Quiz/Modules/Entity/MockDataColors.swift
+++ Quiz/Modules/Entity/MockDataColors.swift
@@ -10,22 +10,22 @@ import UIKit
 
 enum Colors {
     
-    static let BacgroundViewColor = UIColor(red: 248/255, green: 215/255, blue: 155/255, alpha: 1)
+    static let BacgroundViewColor = setupColor(red: 248, green: 255, blue: 228, alpha: 1)
     
     enum CircularProgress {
-        static let GreenColor = UIColor(red: 220/255, green: 255/255, blue: 147/255, alpha: 1)
-        static let OrangeColor = UIColor(red: 255/255, green: 230/255, blue: 166/255, alpha: 1)
-        static let RedColor = UIColor(red: 255/255, green: 38/255, blue: 38/255, alpha: 1)
+        static let GreenColor = setupColor(red: 220, green: 255, blue: 147, alpha: 1)
+        static let OrangeColor = setupColor(red: 255, green: 230, blue: 166, alpha: 1)
+        static let RedColor = setupColor(red: 255, green: 38, blue: 38, alpha: 1)
     }
     
     enum ButtonsMainView {
-        static let BlueColor = UIColor(red: 77/255, green: 171/255, blue: 247/255, alpha: 1)
-        static let BlueBorderColor = UIColor(red: 34/255, green: 139/255, blue: 230/255, alpha: 1)
-        static let WhiteColor = UIColor(red: 248/255, green: 215/255, blue: 155/255, alpha: 1)
-        static let WhiteBorderColor = UIColor(red: 199/255, green: 215/255, blue: 155/255, alpha: 1)
+        static let BlueColor = setupColor(red: 77, green: 171, blue: 247, alpha: 1)
+        static let BlueBorderColor = setupColor(red: 34, green: 139, blue: 230, alpha: 1)
+        static let WhiteColor = setupColor(red: 248, green: 215, blue: 155, alpha: 1)
+        static let WhiteBorderColor = setupColor(red: 199, green: 215, blue: 155, alpha: 1)
     }
     
-    private func setupColor(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+    private static func setupColor(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
         UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
     }
 }
